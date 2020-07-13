929a55046246e710b844b05842f9de6d40e40a7e
jimenezh <jimenezshr@fb.com>
2020-07-13 15:09:45 -0700

    Added adapter

--- app/src/main/java/com/example/voteboat/fragments/ElectionFeedFragment.java
+++ app/src/main/java/com/example/voteboat/fragments/ElectionFeedFragment.java
@@ -8,12 +8,20 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import com.example.voteboat.adapters.ElectionFeedAdapter;
 import com.example.voteboat.databinding.FragmentElectionFeedBinding;
+import com.example.voteboat.models.Election;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ElectionFeedFragment extends Fragment {
 
     FragmentElectionFeedBinding binding;
+    ElectionFeedAdapter adapter;
+    List<Election> elections;
 
     @Nullable
     @Override
@@ -22,6 +30,16 @@ public class ElectionFeedFragment extends Fragment {
         return binding.getRoot();
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        // Inititalizing empty list
+        elections = new ArrayList<>();
+        // Setting adapter
+        adapter = new ElectionFeedAdapter(getContext(),elections);
+        binding.rvElections.setAdapter(adapter);
+        binding.rvElections.setLayoutManager(new LinearLayoutManager(getContext()));
+    }
 
     @Override
     public void onDestroyView() {
