83306669a19f3d233ff711a7c8096b03c1a5b791
jimenezh <jimenezshr@fb.com>
2020-07-13 14:46:16 -0700

    Navigation logic + simple fragment

--- app/src/main/java/com/example/voteboat/MainActivity.java
+++ app/src/main/java/com/example/voteboat/MainActivity.java
@@ -6,14 +6,20 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import com.example.voteboat.databinding.ActivityMainBinding;
+import com.example.voteboat.fragments.ElectionFeedFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "MainActivity";
     ActivityMainBinding binding;
+    final FragmentManager fragmentManager = getSupportFragmentManager();
+    final ElectionFeedFragment electionFeedFragment = new ElectionFeedFragment();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,23 +29,36 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        // Setting listener for bottom navigation
+        setBottomNavigationListener();
+
+    }
+
+    private void setBottomNavigationListener() {
         binding.bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            Fragment fragment;
+
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
                     case R.id.action_home:
-                        Toast.makeText(MainActivity.this, "Clicked on home",Toast.LENGTH_SHORT).show();
-                        return true;
+                        fragment = electionFeedFragment;
+                        Toast.makeText(MainActivity.this, "Clicked on home", Toast.LENGTH_SHORT).show();
                     case R.id.action_calendar:
-                        Toast.makeText(MainActivity.this, "Clicked on calendar",Toast.LENGTH_SHORT).show();
-                        return true;
+                        fragment = electionFeedFragment;
+                        Toast.makeText(MainActivity.this, "Clicked on calendar", Toast.LENGTH_SHORT).show();
                     case R.id.action_profile:
-                        Toast.makeText(MainActivity.this, "Clicked on profile",Toast.LENGTH_SHORT).show();
-                        return true;
-                    default: return true;
+                        fragment = electionFeedFragment;
+                        Toast.makeText(MainActivity.this, "Clicked on profile", Toast.LENGTH_SHORT).show();
+                    default:
+                        fragment = electionFeedFragment;
+                        break;
                 }
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                return true;
             }
         });
 
+        binding.bottomNavigation.setSelectedItemId(R.id.action_home);
     }
 }
