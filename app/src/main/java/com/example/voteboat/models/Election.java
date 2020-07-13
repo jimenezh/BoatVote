17439b96faa2b118be40ce5395545044c5378641
jimenezh <jimenezshr@fb.com>
2020-07-13 15:09:31 -0700

    Dummy election class

---
+++ app/src/main/java/com/example/voteboat/models/Election.java
@@ -0,0 +1,13 @@
+package com.example.voteboat.models;
+
+public class Election {
+    String title;
+
+    public Election(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
