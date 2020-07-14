ffe1508c02545dc43c7f8af3354596a5ff9a4b3a
jimenezh <jimenezshr@fb.com>
2020-07-14 08:50:41 -0700

    Gets user's location

--- app/src/main/java/com/example/voteboat/MainActivity.java
+++ app/src/main/java/com/example/voteboat/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.voteboat;
 
+import android.Manifest;
+import android.location.Location;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.Toast;
 
@@ -11,14 +15,25 @@ import androidx.fragment.app.FragmentManager;
 
 import com.example.voteboat.databinding.ActivityMainBinding;
 import com.example.voteboat.fragments.ElectionFeedFragment;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.RuntimePermissions;
+
+import static com.google.android.gms.location.LocationServices.getFusedLocationProviderClient;
+
+@RuntimePermissions // using https://github.com/permissions-dispatcher
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "MainActivity";
     ActivityMainBinding binding;
     final FragmentManager fragmentManager = getSupportFragmentManager();
     final ElectionFeedFragment electionFeedFragment = new ElectionFeedFragment();
+
+    public FusedLocationProviderClient fusedLocationProviderClient;
 
 
     @Override
@@ -32,6 +47,47 @@ public class MainActivity extends AppCompatActivity {
         // Setting listener for bottom navigation
         setBottomNavigationListener();
 
+        // Location
+        // Checking if key is null
+        if (TextUtils.isEmpty(getResources().getString(R.string.google_maps_api_key))) {
+            throw new IllegalStateException("You forgot to supply a Google Maps API key");
+        }
+        // Generated class method to request permission for location
+        MainActivityPermissionsDispatcher.getLocationWithPermissionCheck(this);
+        getLocation();
+
+    }
+
+    // Delegates work to helper class
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        MainActivityPermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults);
+    }
+
+    // Annotations from dependency. Includes fine + coarse location
+    @SuppressWarnings({"MissingPermission"})
+    @NeedsPermission({Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION})
+    protected void getLocation() {
+        // Google API to get location
+        FusedLocationProviderClient locationClient = getFusedLocationProviderClient(this);
+        locationClient.getLastLocation()
+                .addOnSuccessListener(new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Now that we have the location, we go to this function
+                        Toast.makeText(MainActivity.this,"Got location", Toast.LENGTH_LONG).show();
+                        Log.i(TAG, "Location is "+location.toString());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d(TAG, "Error trying to get last GPS location");
+                        Toast.makeText(MainActivity.this,"No location", Toast.LENGTH_LONG).show();
+                        e.printStackTrace();
+                    }
+                });
     }
 
     private void setBottomNavigationListener() {
