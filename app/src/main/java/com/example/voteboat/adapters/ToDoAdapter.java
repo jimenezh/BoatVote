c480821b4a60e45a102532169ec33d7b7acd1c72
jimenezh <jimenezshr@fb.com>
2020-07-22 14:28:17 -0700

    Transferred methods to adapter

--- app/src/main/java/com/example/voteboat/adapters/ToDoAdapter.java
+++ app/src/main/java/com/example/voteboat/adapters/ToDoAdapter.java
@@ -1,20 +1,34 @@
 package com.example.voteboat.adapters;
 
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.FileProvider;
+import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.voteboat.activities.MainActivity;
 import com.example.voteboat.databinding.ItemTodoBinding;
 import com.example.voteboat.fragments.PictureFragment;
 import com.example.voteboat.models.ToDoItem;
+import com.facebook.FacebookSdk;
+import com.facebook.share.model.SharePhoto;
+import com.facebook.share.model.SharePhotoContent;
+import com.facebook.share.widget.ShareButton;
 import com.like.LikeButton;
 import com.like.OnLikeListener;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.util.List;
 
@@ -26,10 +40,15 @@ public class ToDoAdapter extends RecyclerView.Adapter<ToDoAdapter.ViewHolder> {
 
     public static final String TAG = "ToDoAdapter";
 
+    public interface OnActivityResultListener{
+        int activityResult();
+    }
+
 
     public ToDoAdapter(Context context, List<ToDoItem> elections) {
         this.context = context;
         this.toDoItems = elections;
+        FacebookSdk.fullyInitialize();
     }
 
     @NonNull
@@ -52,6 +71,11 @@ public class ToDoAdapter extends RecyclerView.Adapter<ToDoAdapter.ViewHolder> {
     class ViewHolder extends RecyclerView.ViewHolder {
 
         ItemTodoBinding binding;
+        // For camera
+        private File photoFile;
+        public String photoFileName = "photo.jpg";
+        public final static int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 42;
+
 
         public ViewHolder(@NonNull ItemTodoBinding itemTodoBinding) {
             super(itemTodoBinding.getRoot());
@@ -71,7 +95,8 @@ public class ToDoAdapter extends RecyclerView.Adapter<ToDoAdapter.ViewHolder> {
             binding.btnCamera.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    ((MainActivity) context).setElectionListener(null, new PictureFragment(),"none");
+                    launchCamera();
+//                    ((MainActivity) context).setElectionListener(null, new PictureFragment(),"none");
                 }
             });
         }
@@ -128,6 +153,60 @@ public class ToDoAdapter extends RecyclerView.Adapter<ToDoAdapter.ViewHolder> {
                         item.setRegistered(false);
                 }
             });
+        }
+
+        private void launchCamera() {
+            // create Intent to take a picture and return control to the calling application
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            // Create a File reference for future access
+            photoFile = getPhotoFileUri(photoFileName);
+
+            // wrap File object into a content provider
+            // required for API >= 24
+            // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
+            Uri fileProvider = FileProvider.getUriForFile(context, "com.voteboat.fileprovider", photoFile);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+
+            // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+            // So as long as the result is not null, it's safe to use the intent.
+            if (intent.resolveActivity(context.getPackageManager()) != null) {
+                // Start the image capture intent to take photo
+                ((MainActivity) context).startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+            }
+        }
+        private File getPhotoFileUri(String fileName) {        // Get safe storage directory for photos
+            // Use `getExternalFilesDir` on Context to access package-specific directories.
+            // This way, we don't need to request external read/write runtime permissions.
+            File mediaStorageDir = new File(context.getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG);
+
+            // Create the storage directory if it does not exist
+            if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()) {
+                Log.d(TAG, "failed to create directory");
+            }
+
+            // Return the file target for the photo based on filename
+            File file = new File(mediaStorageDir.getPath() + File.separator + fileName);
+
+            return file;
+
+        }
+        private void prepareToPublishImage() {
+            Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+            setFacebookButtonContent(takenImage,"description");
+        }
+
+        private void setFacebookButtonContent(Bitmap takenImage, String caption) {
+            // SharePhoto is Facebook's data model for photos
+            SharePhoto photo = new SharePhoto.Builder()
+                    .setBitmap(takenImage)
+                    .setCaption(caption)
+                    .build();
+            // Add the photo to the post's content
+            SharePhotoContent content = new SharePhotoContent.Builder()
+                    .addPhoto(photo)
+                    .build();
+            // Triggers intent to the Facebook app
+            ((ShareButton) binding.btnShare).setShareContent(content);
         }
     }
 }
