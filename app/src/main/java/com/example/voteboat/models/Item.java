94432d9e9b3a386628f5d4f197ceeeae80355c9d
jimenezh <jimenezshr@fb.com>
2020-07-23 13:40:54 -0700

    Data model for MultiLevelRV

---
+++ app/src/main/java/com/example/voteboat/models/Item.java
@@ -0,0 +1,9 @@
+package com.example.voteboat.models;
+
+import com.multilevelview.models.RecyclerViewItem;
+
+public class Item extends RecyclerViewItem {
+    public Item(int level) {
+        super(level);
+    }
+}
