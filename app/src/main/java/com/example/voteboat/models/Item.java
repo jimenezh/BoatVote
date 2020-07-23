8bbbb321f5a6a65f7a3e8c3256d5bb9c9d9118a8
jimenezh <jimenezshr@fb.com>
2020-07-23 13:56:40 -0700

    Three types of items: label, ToDoItem, Representative

--- app/src/main/java/com/example/voteboat/models/Item.java
+++ app/src/main/java/com/example/voteboat/models/Item.java
@@ -3,7 +3,40 @@ package com.example.voteboat.models;
 import com.multilevelview.models.RecyclerViewItem;
 
 public class Item extends RecyclerViewItem {
-    public Item(int level) {
+    static Enum TODO;
+    static Enum REP;
+    static Enum LABEL;
+
+    ToDoItem toDoItem;
+    Representative representative;
+    String label;
+    Enum type;
+
+
+
+    public Item(int level, ToDoItem toDoItem) {
         super(level);
+        this.type = TODO;
+        this.toDoItem = toDoItem;
+    }
+
+    public Item(int level, String label) {
+        super(level);
+        this.type = LABEL;
+        this.label = label;
+    }
+
+    public Item(int level, Representative representative) {
+        super(level);
+        this.representative = representative;
+        this.type = REP;
+    }
+
+    public ToDoItem getToDoItem() {
+        return toDoItem;
+    }
+
+    public Representative getRepresentative() {
+        return representative;
     }
 }
